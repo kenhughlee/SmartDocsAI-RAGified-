80da3a15cf1140e166056e57cda7a2371aecaa74
kenhughlee <khughlee@gmail.com>
2025-01-08 10:19:59 -0800

    updated llama model to get access to hugging face

--- backend/llama_model.py
+++ backend/llama_model.py
@@ -1,12 +1,18 @@
 import os
 from transformers import LlamaTokenizer, LlamaForCausalLM
+from dotenv import load_dotenv
+from huggingface_hub import login
+
+load_dotenv()
+login(os.getenv('HUGGINGFACE_TOKEN'))
+MODEL_PATH = "meta-llama/Llama-2-7b-chat-hf"
 
 class LlamaChat:
     def __init__(self):
-        #.env file needs to be created and LLAMA model path needs to be added.
-        #dev needs to get LLAMA model path after getting access eg. MODEL_PATH = "meta-llama/Llama-2-7b-chat-hf"
-        self.tokenizer = LlamaTokenizer.from_pretrained(os.environ.get("MODEL_PATH"))
-        self.model = LlamaForCausalLM.from_pretrained(os.environ.get("MODEL_PATH"))
+        #.env file needs to be created and LLAMA model path and access token needs to be added.
+        #dev needs to login to HuggingFace to get access to the model path. A token will be given if approved
+        self.tokenizer = LlamaTokenizer.from_pretrained(MODEL_PATH,use_auth_token=os.getenv('HUGGINGFACE_TOKEN'))
+        self.model = LlamaForCausalLM.from_pretrained(MODEL_PATH,use_auth_token=os.getenv('HUGGINGFACE_TOKEN'))
 
     def generate(self, query, context):
         input_text = f"Context: {context}\nQuestion: {query}\nAnswer:"
